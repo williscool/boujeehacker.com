7486fda28812c56f4bdb77dd2f9fe862e22b3520
William Harris <wharris@upscalews.com>
2026-06-07 22:46:30 +0000

    build: bump node version
    also ignore unimportant error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.scss";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
