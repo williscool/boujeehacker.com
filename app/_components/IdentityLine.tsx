42100362b73bc1bbb70528e8cc235ed9959981ce
William Harris <wharris@upscalews.com>
2026-06-08 04:33:36 +0000

    fix: maybe links

--- app/_components/IdentityLine.tsx
+++ app/_components/IdentityLine.tsx
@@ -3,8 +3,17 @@ import MarkdownLinkInBlank from "./MarkdownLinkInBlank";
 
 export default function IdentityLine({ markdown }: { markdown: string }) {
   return (
-    <div className="font-display text-3xl sm:text-4xl leading-snug text-text [&_p]:m-0 [&_strong]:font-normal [&_a]:underline [&_a]:decoration-1 [&_a]:underline-offset-[4px] [&_a]:decoration-border hover:[&_a:hover]:text-link-hover hover:[&_a:hover]:decoration-link-hover">
-      <ReactMarkdown components={{ a: MarkdownLinkInBlank }}>
+    <div className="font-display text-3xl sm:text-4xl leading-snug text-text [&_p]:m-0 [&_strong]:font-normal">
+      <ReactMarkdown
+        components={{
+          a: (props) => (
+            <MarkdownLinkInBlank
+              {...props}
+              className="underline decoration-1 underline-offset-[4px] decoration-border hover:text-link-hover hover:decoration-link-hover"
+            />
+          ),
+        }}
+      >
         {markdown}
       </ReactMarkdown>
     </div>
